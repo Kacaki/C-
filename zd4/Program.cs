f3a52858e9322b2eccf59b360769be444fe5237f
Roman <ro@bk.ru>
2023-02-14 23:44:18 +0500

    домашнее задание №4

---
+++ zd4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("введите число 1 ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите число 2 ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите число 3 ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+
+int max;
+
+if (a>b) max=a;
+if (a>c) max=a;
+if (c>b) max=c;
+else max=b;
+
+Console.Write("Масимальное число равно ");
+Console.Write(max);
+
