c7b9076165799602a58091be3485714a9336dec3
Roman <ro@bk.ru>
2023-02-14 21:15:52 +0500

    добавили проект по проверке имени

---
+++ exmpl_if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введиет ваше имя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "алёна")
+{
+    Console.WriteLine("пупсик с Днем Валентина");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
