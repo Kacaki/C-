8419e7ccbf5994a839f1232951c57f3427e3b17c
Roman <ro@bk.ru>
2023-02-14 23:01:06 +0500

    Выполнение задачи №2

---
+++ zd2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("введите число 1 ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите число 2 ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max;
+
+if (a>b) max=a;
+
+else max=b;
+
+Console.Write("Масимальнео число равно ");
+Console.Write(max);
+
