6280adb0d451ddb8e7e91b82ecfb1343f2614819
Roman <ro@bk.ru>
2023-02-15 09:24:27 +0500

    задача №8

---
+++ zd8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+if (a>0)
+    for (int b = 1;b<=a; b++)
+        
+        if (b % 2==0)
+            Console.WriteLine(b);
+        
+if (a<0) 
+    for (int b = -1;b>=a; b=b-1)
+        
+        if (b % 2==0)
+            Console.WriteLine(b);
