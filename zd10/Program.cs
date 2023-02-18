ac96dafa7c9758c310a27f2ad2aefc42f87b7e15
Roman <ro@bk.ru>
2023-02-18 19:02:59 +0500

    задачи №10 и № 13 домашнего задания по c#

---
+++ zd10/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Введите число");
+int b = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Какую по порядку цифру введенного числа вывести?");
+int a = int.Parse(Console.ReadLine());
+
+int lenght = b.ToString().Length; 
+
+if (lenght >=a )
+    {
+    string str = b.ToString();
+    Console.WriteLine(str[a-1]);
+    }
+   else 
+    Console.WriteLine("Такой цифры нет");
