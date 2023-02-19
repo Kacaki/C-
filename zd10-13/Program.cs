7af02e5deb2a4a2ee9efe339371862925a5db3f8
Roman <ro@bk.ru>
2023-02-19 16:03:25 +0500

    решил задачи №10 и 13 с помощью метода.

---
+++ zd10-13/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int b = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Какую по порядку цифру введенного числа вывести?");
+int a = int.Parse(Console.ReadLine());
+
+digt(a, b);
+
+void digt(int x, int y)
+{
+        int lenght = y.ToString().Length; 
+
+        if (lenght >=x )
+            {
+            string str = y.ToString();
+            Console.Write("Вы выбрали цифру {0}", str[x-1]);
+            }
+        else 
+            Console.WriteLine("Такой цифры нет");
+
+}
--- zd10/Program.cs
+++ zd10/Program.cs
@@ -6,7 +6,6 @@ int b = int.Parse(Console.ReadLine());
 Console.WriteLine("Какую по порядку цифру введенного числа вывести?");
 int a = int.Parse(Console.ReadLine());
 
-
 int lenght = b.ToString().Length; 
 
 if (lenght >=a )
