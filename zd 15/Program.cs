94c7696c24c97cc58cb4341b2b7ccc955853be61
Roman <ro@bk.ru>
2023-02-19 16:26:32 +0500

    решил задачу №15

---
+++ zd 15/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите порядковый день недели в диапазоне от 1 до 7");
+int a = int.Parse(Console.ReadLine());
+
+day(a);
+
+void day(int a)
+        {
+            
+            if (a > 0 && a < 6)
+            Console.Write("Это будний день");
+            
+            else if (a > 7)
+            Console.Write("В неделе всего 7 дней");
+            
+            else 
+            Console.Write("Это выходной день"); 
+
+
+        }
