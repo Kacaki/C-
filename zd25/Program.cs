fef244f41abd7a08b233bc679f3ba2ca57f62f25
Roman <ro@bk.ru>
2023-02-22 13:28:42 +0500

    решил задачу 25

---
+++ zd25/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.Clear();
+
+void Digt(int n, int i)
+
+{
+    
+        
+         double d = Math.Pow(n, i);
+            Console.WriteLine("Куб " + d);
+     
+}
+
+
+void Main()
+  {
+
+    Console.WriteLine("Введите 1ое число");
+    int n = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите 2ое число");
+    int i = int.Parse(Console.ReadLine());
+    
+    Digt(n,i);
+
+   }
+
+Main();
+
+
