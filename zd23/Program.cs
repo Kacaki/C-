7940e28441acbc3dd84a1016f24206da0d225ca0
Roman <ro@bk.ru>
2023-02-22 12:50:43 +0500

    сделала задачу №23

---
+++ zd23/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+void Digt(int n)
+
+{
+    
+    for (int i=1; i != n+1; i++)
+     {   
+         double d = Math.Pow(i, 3);
+            Console.WriteLine("Куб " + d);
+     }
+}
+
+
+
+
+
+void Main()
+  {
+
+    Console.WriteLine("Введите число N");
+    int n = int.Parse(Console.ReadLine());
+
+    Digt(n);
+
+   }
+
+Main();
