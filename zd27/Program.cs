484bbe36e76222032382bcc70f6a4ec5f09563e9
Roman <ro@bk.ru>
2023-02-24 12:40:03 +0500

    добавил задачу №27

---
+++ zd27/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.Clear();
+
+
+
+    void Digt(int n)
+        {
+            int s = 0;
+            while(n!=0)
+            {
+                s = s + n % 10;
+                n = n / 10;
+            }
+            Console.WriteLine("Сумма чисел равна: {0}",s);
+        }
+
+
+
+ void Main()
+  {
+
+    Console.WriteLine("Введите число");
+    int n = int.Parse(Console.ReadLine());
+    
+    Digt(n);
+
+   }
+
+Main();           
