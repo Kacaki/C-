3bc4a3120c2fa636e4ccac3e6e0df089155c05f3
Roman <ro@bk.ru>
2023-02-24 13:48:56 +0500

    решил 29 задачу

---
+++ zd29/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console.Clear();
+
+void Array(int[] collection)
+{
+    int length = collection.Length;
+    int index=0;
+    while (index < length)
+{
+    collection[index] = new Random().Next(1,8);
+    index++;
+}
+
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    Console.WriteLine("Выводим случайно заданный массив из 8 чисел");
+    while (position < count)
+    {
+        Console.Write(col[position]);
+        position++;
+
+    }
+}
+
+ int[] array = new int[8];
+
+Array(array);
+PrintArray(array);
