62cdaabe2c5ba612a63b8ed1ba2f34c3385f1896
Roman <ro@bk.ru>
2023-02-25 20:42:16 +0500

    сделал задачу №34

---
+++ zd34/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.Clear();
+
+void Array(int[] collection)
+{
+    int length = collection.Length;
+    int index=0;
+    while (index < length)
+{
+    collection[index] = new Random().Next(100,1000);
+    index++;
+}
+
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    Console.WriteLine("Выводим случайно заданный массив из 8 чисел");
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+
+    }
+}
+
+
+void EvenArray(int[] arr)
+    {
+    int length = arr.Length;
+    int index=0;
+    int count=0;
+    while (index < length)
+{
+    if (arr[index] % 2==0)
+        count++;
+        index++;
+}
+        Console.WriteLine("Количество чётных чисел в массиве " +count);
+}
+
+
+
+
+
+
+ int[] array = new int[8];
+
+Array(array);
+PrintArray(array);
+EvenArray(array);
