48124c65c17f23046eb4b3bb74ea74c2b8520b84
Roman <ro@bk.ru>
2023-02-25 21:02:14 +0500

    решил задачу 36

---
+++ zd36/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.Clear();
+
+void Array(int[] collection)
+{
+    int length = collection.Length;
+    int index=0;
+    while (index < length)
+{
+    collection[index] = new Random().Next(-100,100);
+    index++;
+}
+
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    Console.WriteLine("Выводим случайно заданный массив из 8 чисел");
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+
+    }
+}
+
+
+void SumArray(int[] arr)
+    {
+    int length = arr.Length;
+    int index=0;
+    int sum=0;
+    while (index < length)
+{
+    if (index % 2==0)
+        sum = (sum +arr[index]);
+        index++;
+}
+        Console.WriteLine("Сумма чисел на четных индексах массива " +sum);
+}
+
+
+
+ int[] array = new int[8];
+
+Array(array);
+PrintArray(array);
+SumArray(array);
