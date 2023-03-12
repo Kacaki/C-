5b30978dd76fc466058624a7bb634e7a2bfe481b
Roman <ro@bk.ru>
2023-03-12 17:06:34 +0500

    сделал задачу 64

---
+++ zd64/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+void Digt(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        Digt(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
+
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+Digt(n, count);
