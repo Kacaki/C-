11fe8d9024da7eb00d25cd56ae917b44333f3885
Roman <ro@bk.ru>
2023-03-12 17:16:40 +0500

    решил задачу 66

---
+++ zd66/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+int Sum(int m, int n)
+{
+    int result = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        result = m + Sum(m, n);
+        return result;
+    }
+}
+
+
+
+void PrintSum(int m, int n)
+{
+    Console.Write(Sum(m - 1, n));
+}
+
+
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+PrintSum(m, n);
