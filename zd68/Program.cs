9676d6234ccdd7b43e912726d450b5fe8a6aea9d
Roman <ro@bk.ru>
2023-03-12 17:27:10 +0500

    решил задачу 68

---
+++ zd68/Program.cs
@@ -0,0 +1,35 @@
+﻿
+Console.Clear();
+
+
+int Akk(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akk(m - 1, 1);
+    }
+    else
+    {
+        return (Akk(m - 1, Akk(m, n - 1)));
+    }
+}
+
+
+
+void AkkFunk(int m, int n)
+{
+    Console.Write(Akk(m, n)); 
+}
+
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AkkFunk(m,n);
